2223c402b4ab8a85050ae49efa1642fe6dc5cd5c
Sean McKinney <seanamckinney8@gmail.com>
2023-01-25 10:45:22 -0600

    added an override to GetLetterGade in RankedGradeBook.cs.  Filled out method with LINQ to oder Students and then Select by AverageGrade and put ToList.  Added algorithm and if, else if's, else to assign grades based on position in the class.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Transactions;
 
 namespace GradeBook.GradeBooks
 {
@@ -9,6 +12,36 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("There must be at least 5 students for this action to work.");
+
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades[threshold - 1] <= averageGrade)
+            {
+                return 'A';
+            }
+            else if (grades[(threshold * 2) -1] <= averageGrade)
+            {
+                return 'B';
+            }
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
+            {
+                return 'C';
+            }
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
         }
     }
 }
