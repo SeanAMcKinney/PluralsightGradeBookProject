cd39a294d6565f6634c9f07cf5746259088406fa
Sean McKinney <seanamckinney8@gmail.com>
2023-01-25 11:28:38 -0600

    Created a new public property named IsWeighted of type bool in the BaseGradeBook class. Added a bool to the BaseGradeBook's, StandardGradeBook's, and RankedGradeBook's Constructors. In the StartingUserInterface's CreateCommand method update where the gradebooks are instantiated to use parts[3] to pass a bool as the last argument of each instantiation. In BaseGradeBook's constructor, set the IsWeighted property using the bool parameter. In the StartingUserInterface's CreateCommand method, update the condition that checks if parts is not equal to 3 to instead check if parts is not equal to 4. Changed when a grade book's IsWeighted property is true and the student type is Honors or DualEnrolled, to add 1 to the GPA before returning it.

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,11 +14,14 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
             Students = new List<Student>();
+            IsWeighted = isWeighted;
+
         }
 
         public void AddStudent(Student student)
@@ -110,15 +113,50 @@ namespace GradeBook.GradeBooks
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    if (studentType == StudentType.Honors || studentType == StudentType.Honors)
+                    {
+                        return 5;
+                    }
+                    else
+                    {
+                        return 4;
+                    }
                 case 'B':
-                    return 3;
+                    if (studentType == StudentType.Honors || studentType == StudentType.Honors)
+                    {
+                        return 4;
+                    }
+                    else
+                    {
+                        return 3;
+                    }
                 case 'C':
-                    return 2;
+                    if (studentType == StudentType.Honors || studentType == StudentType.Honors)
+                    {
+                        return 3;
+                    }
+                    else
+                    {
+                        return 2;
+                    }
                 case 'D':
-                    return 1;
+                    if (studentType == StudentType.Honors || studentType == StudentType.Honors)
+                    {
+                        return 2;
+                    }
+                    else
+                    {
+                        return 1;
+                    }
                 case 'F':
-                    return 0;
+                    if (studentType == StudentType.Honors || studentType == StudentType.Honors)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return 0;
+                    }
             }
             return 0;
         }
@@ -264,7 +302,7 @@ namespace GradeBook.GradeBooks
                              from type in assembly.GetTypes()
                              where type.FullName == "GradeBook.GradeBooks.StandardGradeBook"
                              select type).FirstOrDefault();
-            
+
             return JsonConvert.DeserializeObject(json, gradebook);
         }
     }
